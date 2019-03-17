68e9e042ec909a0bc4bb5cc9866130487efffc37
che@pellucidit.com <che@pellucidit.com>
2019-03-17 17:01:41 -0400

    Added page footer

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
+    // Code to attempt to prevent downloads
+    // $('#videoElementID').bind('contextmenu',function() { return false; });
 });
 
