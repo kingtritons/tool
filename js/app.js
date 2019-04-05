133e250fdff3a411f49dc3bca8e09951e7b00ede
che@pellucidit.com <che@pellucidit.com>
2019-04-05 13:30:40 -0400

    Updated style and fixed quiz bug

--- js/app.js
+++ js/app.js
@@ -3,157 +3,332 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.slider').slider();
+    $('.modal').modal();
+    $(".preloader-wrapper").hide();
+    let count = 0;
+
     // Code to attempt to prevent downloads
     // $('#videoElementID').bind('contextmenu',function() { return false; });
 
-    let questions =
+    // let questions =
+    //     [
+    //         {
+    //             question: "What is a sequential circuit?",
+    //             A: "A type of computer.",
+    //             B: "A circuit whose output depends on the order or timing of the inputs.",
+    //             C: "A way of simplifying Boolean Algebra.",
+    //             D: "Another name for a combinational circuit.",
+    //             answer: 'B'
+    //         },
+    //         {
+    //             question: "What is a flip flop?",
+    //             A: "A circuit that has two stable states and can be used to store state information.",
+    //             B: "The internal fan of a computer.",
+    //             C: "Another name for a CPU",
+    //             D: "Another name for 0 addressing",
+    //             answer: 'A'
+    //         },
+    //         {
+    //             question: "Which flip flop mimics an exclusive OR in the characteristic table?",
+    //             A: "JK.",
+    //             B: "D",
+    //             C: "T",
+    //             D: "None of the above",
+    //             answer: 'C'
+    //         },
+    //         {
+    //             question: "Which Flip Flop has two inputs(excluding the clock)?",
+    //             A: "JK",
+    //             B: "D",
+    //             C: "T",
+    //             D: "D & T",
+    //             answer: 'A'
+    //         },
+    //         {
+    //             question: "Which counters use a clock signal?",
+    //             A: "Asynchronous ",
+    //             B: "Synchronous",
+    //             C: "Both",
+    //             D: "None",
+    //             answer: 'B'
+    //         },
+    //         {
+    //             question: "A 3 bit synchronous counter would use how many flip flops?",
+    //             A: "2",
+    //             B: "1",
+    //             C: "3",
+    //             D: "As much as you like",
+    //             answer: 'C'
+    //         },
+    //         {
+    //             question: "A bidirectional counter has a sequence: 0, 3, 5, 4, 1, 2, 7, 6. How many flip flips does this counter have?",
+    //             A: "4",
+    //             B: "3",
+    //             C: "7",
+    //             D: "None of the above",
+    //             answer: 'B'
+    //         },
+    //         {
+    //             question: "A T flip flop has A`B + AB` as its input. What other logic gate could be used in replacement of the two AND gates and the OR gate?",
+    //             A: "NOR",
+    //             B: "XOR",
+    //             C: "NOT",
+    //             D: "NAND",
+    //             answer: 'B'
+    //         },
+    //         {
+    //             question: "Which tables are needed to design any synchronous counter? (Assuming we want full marks)",
+    //             A: "Excitation Table",
+    //             B: "Excitation & Present-State, Next-State Tables",
+    //             C: "Characteristic & Present-State, Next-State Tables",
+    //             D: "Characteristics, Excitation & Present-State, Next-State Tables",
+    //             answer: 'D'
+    //         },
+    //         {
+    //             question: "What is the binary representation of the sequence: 3, 1, 0, 2?",
+    //             A: "11, 10, 00, 01",
+    //             B: "10, 11, 00, 01",
+    //             C: "11, 01, 00, 10",
+    //             D: "01, 11, 10, 00",
+    //             answer: 'C'
+    //         },
+    //         {
+    //             question: "What is a counter?",
+    //             A: "A program used to test computer performance.",
+    //             B: "A device used to store the number of occurrences of a particular event.",
+    //             C: "A device used to perform calculations.",
+    //             D: "A circuit that changes code to signals",
+    //             answer: 'B'
+    //         },
+    //         {
+    //             question: "What is the difference between a synchronous and asynchronous counter?",
+    //             A: "Inputs",
+    //             B: "Clock Speed",
+    //             C: "Clock Signals",
+    //             D: "A and C",
+    //             answer: 'D'
+    //         },
+    //         {
+    //             question: "What are synchronous counters made of?",
+    //             A: "Flip Flops and logic gates.",
+    //             B: "Flip Flops only",
+    //             C: "Multiplexers and Decoders",
+    //             D: "None of the above",
+    //             answer: 'A'
+    //         },
+    //         {
+    //             question: "What is a logic gate?",
+    //             A: "A type of memory",
+    //             B: "A part of the ALU",
+    //             C: "A device which interprets machine language",
+    //             D: "A switch which implements boolean logic",
+    //             answer: 'D'
+    //         },
+    //         {
+    //             question: "A Flip Flop is also known as a:",
+    //             A: "Bi-stable circuit",
+    //             B: "Latch",
+    //             C: "Parallel circuit",
+    //             D: "Tri-stable circuit",
+    //             answer: 'A'
+    //         },
+    //         {
+    //             question: "Which is not a type of flip flop?",
+    //             A: "JK",
+    //             B: "D",
+    //             C: "T",
+    //             D: "Z",
+    //             answer: 'D'
+    //         },
+    //         {
+    //             question: "How many binary digits would be required when representing a sequence which has the largest value of 7?",
+    //             A: "7",
+    //             B: "128",
+    //             C: "14",
+    //             D: "14",
+    //             answer: 'D'
+    //         },
+    //         {
+    //             question: "What value does a clock have at the rising edge?",
+    //             A: "2",
+    //             B: "1",
+    //             C: "0",
+    //             D: "4",
+    //             answer: 'B'
+    //         },
+    //
+    //     ];
+
+    let quiz1 =
         [
             {
                 question: "What is a sequential circuit?",
-                optionA: "A type of computer.",
-                optionB: "A circuit whose output depends on the order or timing of the inputs.",
-                optionC: "A way of simplifying Boolean Algebra.",
-                optionD: "Another name for a combinational circuit.",
-                answer: 'B'
+                A: "A type of computer.",
+                B: "A circuit whose output depends on the order or timing of the inputs.",
+                C: "A way of simplifying Boolean Algebra.",
+                D: "Another name for a combinational circuit.",
+                answer: 'B',
+                answerText: "A circuit whose output depends on the order or timing of the inputs."
             },
             {
                 question: "What is a flip flop?",
-                optionA: "A circuit that has two stable states and can be used to store state information.",
-                optionB: "The internal fan of a computer.",
-                optionC: "Another name for a CPU",
-                optionD: "Another name for 0 addressing",
-                answer: 'A'
+                A: "A circuit that has two stable states and can be used to store state information.",
+                B: "The internal fan of a computer.",
+                C: "Another name for a CPU",
+                D: "Another name for 0 addressing",
+                answer: 'A',
+                answerText: "A circuit that has two stable states and can be used to store state information."
             },
             {
                 question: "Which flip flop mimics an exclusive OR in the characteristic table?",
-                optionA: "JK.",
-                optionB: "D",
-                optionC: "T",
-                optionD: "None of the above",
-                answer: 'C'
+                A: "JK.",
+                B: "D",
+                C: "T",
+                D: "None of the above",
+                answer: 'C',
+                answerText: "T"
             },
             {
                 question: "Which Flip Flop has two inputs(excluding the clock)?",
-                optionA: "JK",
-                optionB: "D",
-                optionC: "T",
-                optionD: "D & T",
-                answer: 'A'
+                A: "JK",
+                B: "D",
+                C: "T",
+                D: "D & T",
+                answer: 'A',
+                answerText: "JK"
             },
             {
                 question: "Which counters use a clock signal?",
-                optionA: "Asynchronous ",
-                optionB: "Synchronous",
-                optionC: "Both",
-                optionD: "None",
-                answer: 'B'
+                A: "Asynchronous ",
+                B: "Synchronous",
+                C: "Both",
+                D: "None",
+                answer: 'B',
+                answerText: "Synchronous"
             },
             {
                 question: "A 3 bit synchronous counter would use how many flip flops?",
-                optionA: "2",
-                optionB: "1",
-                optionC: "3",
-                optionD: "As much as you like",
-                answer: 'C'
+                A: "2",
+                B: "1",
+                C: "3",
+                D: "As much as you like",
+                answer: 'C',
+                answerText: "3"
             },
             {
                 question: "A bidirectional counter has a sequence: 0, 3, 5, 4, 1, 2, 7, 6. How many flip flips does this counter have?",
-                optionA: "4",
-                optionB: "3",
-                optionC: "7",
-                optionD: "None of the above",
-                answer: 'B'
+                A: "4",
+                B: "3",
+                C: "7",
+                D: "None of the above",
+                answer: 'B',
+                answerText: "3"
             },
             {
                 question: "A T flip flop has A`B + AB` as its input. What other logic gate could be used in replacement of the two AND gates and the OR gate?",
-                optionA: "NOR",
-                optionB: "XOR",
-                optionC: "NOT",
-                optionD: "NAND",
-                answer: 'B'
+                A: "NOR",
+                B: "XOR",
+                C: "NOT",
+                D: "NAND",
+                answer: 'B',
+                answerText: 'XOR'
             },
             {
                 question: "Which tables are needed to design any synchronous counter? (Assuming we want full marks)",
-                optionA: "Excitation Table",
-                optionB: "Excitation & Present-State, Next-State Tables",
-                optionC: "Characteristic & Present-State, Next-State Tables",
-                optionD: "Characteristics, Excitation & Present-State, Next-State Tables",
-                answer: 'D'
-            },
+                A: "Excitation Table",
+                B: "Excitation & Present-State, Next-State Tables",
+                C: "Characteristic & Present-State, Next-State Tables",
+                D: "Characteristics, Excitation & Present-State, Next-State Tables",
+                answer: 'D',
+                answerText: "Characteristics, Excitation & Present-State, Next-State Tables"
+            }
+        ];
+
+    let quiz2 =
+        [
             {
                 question: "What is the binary representation of the sequence: 3, 1, 0, 2?",
-                optionA: "11, 10, 00, 01",
-                optionB: "10, 11, 00, 01",
-                optionC: "11, 01, 00, 10",
-                optionD: "01, 11, 10, 00",
-                answer: 'C'
+                A: "11, 10, 00, 01",
+                B: "10, 11, 00, 01",
+                C: "11, 01, 00, 10",
+                D: "01, 11, 10, 00",
+                answer: 'C',
+                answerText: "11, 01, 00, 10"
             },
             {
                 question: "What is a counter?",
-                optionA: "A program used to test computer performance.",
-                optionB: "A device used to store the number of occurrences of a particular event.",
-                optionC: "A device used to perform calculations.",
-                optionD: "A circuit that changes code to signals",
-                answer: 'B'
+                A: "A program used to test computer performance.",
+                B: "A device used to store the number of occurrences of a particular event.",
+                C: "A device used to perform calculations.",
+                D: "A circuit that changes code to signals",
+                answer: 'B',
+                answerText: "A device used to store the number of occurrences of a particular event."
             },
             {
                 question: "What is the difference between a synchronous and asynchronous counter?",
-                optionA: "Inputs",
-                optionB: "Clock Speed",
-                optionC: "Clock Signals",
-                optionD: "A and C",
-                answer: 'D'
+                A: "Inputs",
+                B: "Clock Speed",
+                C: "Clock Signals",
+                D: "A and C",
+                answer: 'D',
+                answerText: "A and C"
             },
             {
                 question: "What are synchronous counters made of?",
-                optionA: "Flip Flops and logic gates.",
-                optionB: "Flip Flops only",
-                optionC: "Multiplexers and Decoders",
-                optionD: "None of the above",
-                answer: 'A'
+                A: "Flip Flops and logic gates.",
+                B: "Flip Flops only",
+                C: "Multiplexers and Decoders",
+                D: "None of the above",
+                answer: 'A',
+                answerText: "Flip Flops and logic gates."
             },
             {
                 question: "What is a logic gate?",
-                optionA: "A type of memory",
-                optionB: "A part of the ALU",
-                optionC: "A device which interprets machine language",
-                optionD: "A switch which implements boolean logic",
-                answer: 'D'
+                A: "A type of memory",
+                B: "A part of the ALU",
+                C: "A device which interprets machine language",
+                D: "A switch which implements boolean logic",
+                answer: 'D',
+                answerText: "A switch which implements boolean logic."
             },
             {
                 question: "A Flip Flop is also known as a:",
-                optionA: "Bi-stable circuit",
-                optionB: "Latch",
-                optionC: "Parallel circuit",
-                optionD: "Tri-stable circuit",
-                answer: 'A'
+                A: "Bi-stable circuit",
+                B: "Latch",
+                C: "Parallel circuit",
+                D: "Tri-stable circuit",
+                answer: 'A',
+                answerText: "Bi-stable circuit"
             },
             {
                 question: "Which is not a type of flip flop?",
-                optionA: "JK",
-                optionB: "D",
-                optionC: "T",
-                optionD: "Z",
-                answer: 'D'
+                A: "JK",
+                B: "D",
+                C: "T",
+                D: "Z",
+                answer: 'D',
+                answerText: "Z"
             },
             {
                 question: "How many binary digits would be required when representing a sequence which has the largest value of 7?",
-                optionA: "7",
-                optionB: "128",
-                optionC: "14",
-                optionD: "14",
-                answer: 'D'
+                A: "7",
+                B: "128",
+                C: "14",
+                D: "14",
+                answer: 'D',
+                answerText: "14"
             },
             {
                 question: "What value does a clock have at the rising edge?",
-                optionA: "2",
-                optionB: "1",
-                optionC: "0",
-                optionD: "4",
-                answer: 'B'
-            },
-
+                A: "2",
+                B: "1",
+                C: "0",
+                D: "4",
+                answer: 'B',
+                answerText: "1"
+            }
         ];
+
     // let randNumTracker = [];
     // for (let i = 0; i < 10; i++)
     // {
@@ -170,6 +345,39 @@ $(document).ready(function () {
     //
     //     console.log(randNum);
     // }
+
+    $("#submit-answers").click(function(){
+        let answers = $("form.mult").serializeArray();
+        $(".preloader-wrapper").show();
+        setTimeout(function () {
+            $(".preloader-wrapper").hide();
+        },400);
+        // $('.modal').modal('open');
+        setTimeout(function() {
+            $('.modal').modal('open')}, 500);
+        $('#submit-answers').prop('disabled', true);
+        checkAnswers(answers);
+    });
+    function checkAnswers(data)
+    {
+        for (let i = 0; quiz1.length; i++)
+        {
+            if(quiz1[i].answer === data[i].value)
+            {
+                count++;
+                console.log("correct");
+            }
+            else
+            {
+                $(".collection").append(`<li class="collection-item hoverable">` + quiz1[i].answerText + `</li>`);
+                console.log(quiz1[i].answerText);
+            }
+        }
+        if (count === quiz1.length)
+        {
+
+        }
+    }
 
 });
 
